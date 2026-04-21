ea3b1d0ee2e92ee5b3ed920cf13dc39ebc1115b1
yoycerg <yoicergarcia@liceovvh.cl>
2026-04-21 12:32:17 -0400

    update

---
+++ python/fundamentos/fundamendos/02_estuctura_control/14_funciones_practicas_2.py
@@ -0,0 +1,38 @@
+#Ejercicio 1
+# Calcula experiencia
+
+def multiplica_por_2():
+    return [0, 2, 4, 6, 8, 10]
+
+multiplica_por_2(5)
+# Debe retornar: [0, 2, 4, 6, 8, 10]
+print(multiplica_por_2())
+#Ejercicio 2
+# Analiza publicaciones
+def suma_y_resta():
+    suma_y_resta([120, 115])
+# Imprime: 235 y retorna: 5
+
+#Ejercicio 3
+# Puntaje ajustado
+def sumatoria_menos_longitud():
+    sumatoria_menos_longitud([10, 5, 3, 7])
+# Suma total = 25, longitud = 4, debe retornar: 21
+
+#Ejercico 4
+# Ajusta visualizaciones
+def valores_multiplicados_segundo():
+    valores_multiplicados_segundo([100, 3, 50, 20])
+# Imprime: 4 y retorna: [300, 9, 150, 60]
+
+valores_multiplicados_segundo([100])
+# Imprime: 1 y retorna: []
+
+#Ejercicio 5
+# Genera precio fijo
+def valor_multiplicado_longitud():
+    valor_multiplicado_longitud(5, 2)
+# Debe retornar: [10, 10]
+
+    valor_multiplicado_longitud(7, 5)
+# Debe retornar: [35, 35, 35, 35, 35]
