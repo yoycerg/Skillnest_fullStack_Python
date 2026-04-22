38f92d8db757d2f792aa9b5bf9386fa7d60fe9ed
yoycerg <yoicergarcia@liceovvh.cl>
2026-04-22 14:44:26 -0400

    asd

--- python/fundamentos/fundamendos/02_estuctura_control/14_funciones_practicas_2.py
+++ python/fundamentos/fundamendos/02_estuctura_control/14_funciones_practicas_2.py
@@ -1,3 +1,5 @@
+import os
+
 #Ejercicio 1
 # Calcula experiencia
 
@@ -11,9 +13,17 @@ print(multiplica_por_2(5))
 
 #Ejercicio 2
 # Analiza publicaciones
-def suma_y_resta():
-    suma_y_resta([120, 115])
+
+def suma_y_resta(lista):
+    suma = lista[0] + lista[1]
+    resta = lista[0] - lista[1]
+    print(suma)
+    return resta
+
+resultado = suma_y_resta([120, 115])
+print(resultado)
 # Imprime: 235 y retorna: 5
+
 
 
 #Ejercicio 3
@@ -21,6 +31,9 @@ def suma_y_resta():
 def sumatoria_menos_longitud():
     sumatoria_menos_longitud([10, 5, 3, 7])
 # Suma total = 25, longitud = 4, debe retornar: 21
+suma_total = sumatoria_menos_longitud(sumatoria_menos_longitud([]))
+print(suma_total[1 + 2])
+
 
 #Ejercico 4
 # Ajusta visualizaciones
@@ -39,3 +52,38 @@ def valor_multiplicado_longitud():
 
     valor_multiplicado_longitud(7, 5)
 # Debe retornar: [35, 35, 35, 35, 35]
+
+def limpiarConsola():
+    os.system('cls')
+
+    continuar = True
+    while continuar:
+        print("\n --- ejercicios ---")
+        print("--- ejercicio 1 ---")
+    print(" --- ejercicio 2 ---")
+    print(" --- ejercicio 3 ---")
+    print(" --- ejercicio 4 ---")
+    print(" --- ejercicio 5 ---")
+    opcion = input("\n --- Elige una opción (1-5) o '(0 para salir)'")
+    if opcion == "1":
+        limpiarConsola()
+        print("\nEjecutando ejercicio 1: ")
+        multiplica_por_2()
+    elif opcion == "2":
+        print("\nEjecutando ejercicio 2: ")    
+        suma_y_resta()
+    elif opcion == "3":
+        print("\nEjecutando ejercicio 3: ")
+        sumatoria_menos_longitud()
+    elif opcion == "4":
+        print("\nEjecutando ejercicio 4: ")
+        valores_multiplicados_segundo()
+    elif opcion == "5":
+        print("\nEjecutando ejercicio 5: ")
+        valor_multiplicado_longitud()
+    elif opcion == "0":
+        limpiarConsola()
+        print("Saliendo...")
+        continuar = False
+    else:
+      print("Opción no válida, intenta otra vez")        
